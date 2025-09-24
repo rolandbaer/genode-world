a33ed1eaf1bcd545b8dca582e7e360a767e6182d
Christian Helmuth <christian.helmuth@genode-labs.com>
2025-09-24 08:05:03 +0200

    Remove RtAudio port
    The upstream repo of this abandoned (?) port vanished.
    
    Fixes #413

--- lib/import/import-rtaudio.mk
+++
@@ -1,5 +0,0 @@
-RTAUDIO_PORT_DIR := $(call select_from_ports,rtaudio)
-
-CC_OPT += -D__GENODE_AUDIO__
-
-INC_DIR += $(RTAUDIO_PORT_DIR)/include $(RTAUDIO_PORT_DIR)/include/RtAudio
--- lib/mk/rtaudio.mk
+++
@@ -1,9 +0,0 @@
-include $(REP_DIR)/lib/import/import-rtaudio.mk
-
-LIBS := stdcxx
-
-SRC_CC := RtAudio.cpp
-
-vpath %.cpp $(RTAUDIO_PORT_DIR)/src/lib/rtaudio
-
-CC_CXX_WARN_STRICT =
